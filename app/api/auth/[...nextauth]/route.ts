28a0df47eff9ecd8a85d1ab5f0e66b91fc39b1a0
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-06 20:51:24 +0530

    Added credentials provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,21 @@
-import { NextRequest, NextResponse } from "next/server"
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+const handler = NextAuth({
+    providers: [
+        CredentialsProvider ({
+            name : 'Credentials',
+            credentials :{
+                username : {label : 'email', type:'text',placeholder:'Email'},
+                password : {label : 'password', type:'password',placeholder:'password'},
+            },
+            async authorize(credentials : any){
+                return {
+                    id : "user1"
+                };
+            },
+        })
+    ],
+    secret : process.env.NEXTAUTH_SECRET
+});
 
-export function GET( req : NextRequest , {params}: {params :{nextauth : string[]}}){
-    console.log(params.nextauth)
-    return NextResponse.json({
-        message : "Handler"
-    })
-}
+export {handler as GET, handler as POST}
