be1296f30e32d4eec9a9102320470df7111cae10
pratikprajapati0708 <prajapatipratik292@gmail.com>
2024-04-06 20:38:30 +0530

    Accessing dynamic route segments

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export function GET( req : NextRequest){
+export function GET( req : NextRequest , {params}: {params :{nextauth : string[]}}){
+    console.log(params.nextauth)
     return NextResponse.json({
         message : "Handler"
     })
